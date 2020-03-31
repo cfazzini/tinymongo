0215bba67c579c7c2eb76d799e83e1c8ac49fc80
cfazzini <3694708+cfazzini@users.noreply.github.com>
2020-03-30 22:36:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     install_requires=[
         'tinydb>=3.2.1',
